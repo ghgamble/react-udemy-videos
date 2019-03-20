12f47ac9564f3473a093959d82dfc130593156be
Grace Gamble <grace.h.gamble@gmail.com>
2019-03-20 12:41:07 -0600

    Fixing a Few Things

--- src/components/App.js
+++ src/components/App.js
@@ -18,15 +18,25 @@ class App extends Component {
             this.setState({videos: response.data.items});
       };
       onVideoSelect = (video) => {
-            console.log('From the app!', video);
             this.setState({selectedVideo: video});
       }
       render () {
             return (
                   <div className="ui container">
                         <SearchBar onFormSubmit={this.onTermSubmit} />
-                        <VideoDetail video={this.state.selectedVideo} />
-                        <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+                        <div className="ui grid">
+                              <div className="ui row">
+                                    <div className="eleven wide column">
+                                          <VideoDetail video={this.state.selectedVideo} />
+                                    </div>
+                                    <div className="five wide column">
+                                          <VideoList
+                                                videos={this.state.videos}
+                                                onVideoSelect={this.onVideoSelect}
+                                          />
+                                    </div>
+                              </div>
+                        </div>
                   </div>
             );
       }
